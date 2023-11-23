c8fd4b7eb1f46d0764239b18f28754ef6d5508e4
Yana-Koroliuk <yana.koroliuk1@gmail.com>
2023-11-23 15:45:16 +0200

    Add logs to delete book

--- src/main/java/ua/training/controller/command/admin/DeleteBook.java
+++ src/main/java/ua/training/controller/command/admin/DeleteBook.java
@@ -3,10 +3,13 @@ package ua.training.controller.command.admin;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import ua.training.controller.command.Command;
+import ua.training.model.entity.Book;
+import ua.training.model.entity.User;
 import ua.training.model.service.BookService;
 import ua.training.model.service.UserService;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 public class DeleteBook implements Command {
     private static final Logger logger = LogManager.getLogger();
@@ -26,8 +29,10 @@ public class DeleteBook implements Command {
         }
         boolean result = bookService.deleteBook(Long.parseLong(bookId));
         if (!result) {
+            logger.error("An error occurred when deleting book with id="+bookId);
             return "/error/error.jsp";
         }
+        logger.info("Deleted book with id="+bookId);
         return "redirect:/admin/home?tab=2";
     }
 }
