bf6b5f68b1d49dd2b04b6f24364eb047eb31d8a1
Yana-Koroliuk <yana.koroliuk1@gmail.com>
2023-11-23 15:34:10 +0200

    Add delete book functionality

---
+++ src/main/java/ua/training/controller/command/admin/DeleteBook.java
@@ -0,0 +1,33 @@
+package ua.training.controller.command.admin;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import ua.training.controller.command.Command;
+import ua.training.model.service.BookService;
+import ua.training.model.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class DeleteBook implements Command {
+    private static final Logger logger = LogManager.getLogger();
+    private final UserService userService;
+    private final BookService bookService;
+
+    public DeleteBook(UserService userService, BookService bookService) {
+        this.userService = userService;
+        this.bookService = bookService;
+    }
+
+    @Override
+    public String execute(HttpServletRequest request) {
+        String bookId = request.getParameter("id");
+        if (bookId == null || bookId.equals("")) {
+            return "redirect:/admin/home?tab=2";
+        }
+        boolean result = bookService.deleteBook(Long.parseLong(bookId));
+        if (!result) {
+            return "/error/error.jsp";
+        }
+        return "redirect:/admin/home?tab=2";
+    }
+}
