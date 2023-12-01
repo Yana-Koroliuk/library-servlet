ce56a95dcf263833aaece9acaf4474bda13027f3
Yana-Koroliuk <yana.koroliuk1@gmail.com>
2023-12-01 14:43:38 +0200

    Update EditBookTest.java

--- src/test/java/ua/training/controller/command/admin/EditBookTest.java
+++ src/test/java/ua/training/controller/command/admin/EditBookTest.java
@@ -102,4 +102,56 @@ public class EditBookTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void executeWithUpdateBookFailure() {
+        when(mockedRequest.getParameter("id")).thenReturn("1");
+        when(mockedRequest.getParameter("titleUa")).thenReturn("Заголовок");
+        when(mockedRequest.getParameter("authorsUa")).thenReturn("Автор");
+        when(mockedRequest.getParameter("descriptionUa")).thenReturn("Опис");
+        when(mockedRequest.getParameter("bookLanguageUa")).thenReturn("Українська");
+        when(mockedRequest.getParameter("editionUa")).thenReturn("Видання");
+        when(mockedRequest.getParameter("titleEn")).thenReturn("Title");
+        when(mockedRequest.getParameter("authorsEn")).thenReturn("Author");
+        when(mockedRequest.getParameter("descriptionEn")).thenReturn("Description");
+        when(mockedRequest.getParameter("bookLanguageEn")).thenReturn("Ukrainian");
+        when(mockedRequest.getParameter("editionEn")).thenReturn("Edition");
+        when(mockedRequest.getParameter("publicationDate")).thenReturn("2000-12-12");
+        when(mockedRequest.getParameter("price")).thenReturn("10");
+        when(mockedRequest.getParameter("currency")).thenReturn("uan");
+        when(mockedRequest.getParameter("count")).thenReturn("7");
+        when(mockedBookService.findById(1L)).thenReturn(Optional.of(mockedBook));
+        when(mockedBookService.updateBook(any(Book.class))).thenReturn(false);
+
+        String expected = "/error/error.jsp";
+        String actual = editBook.execute(mockedRequest);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void execute() {
+        String id = "1";
+        when(mockedRequest.getParameter("id")).thenReturn(id);
+        when(mockedRequest.getParameter("titleUa")).thenReturn("Заголовок");
+        when(mockedRequest.getParameter("authorsUa")).thenReturn("Автор");
+        when(mockedRequest.getParameter("descriptionUa")).thenReturn("Опис");
+        when(mockedRequest.getParameter("bookLanguageUa")).thenReturn("Українська");
+        when(mockedRequest.getParameter("editionUa")).thenReturn("Видання");
+        when(mockedRequest.getParameter("titleEn")).thenReturn("Title");
+        when(mockedRequest.getParameter("authorsEn")).thenReturn("Author");
+        when(mockedRequest.getParameter("descriptionEn")).thenReturn("Description");
+        when(mockedRequest.getParameter("bookLanguageEn")).thenReturn("Ukrainian");
+        when(mockedRequest.getParameter("editionEn")).thenReturn("Edition");
+        when(mockedRequest.getParameter("publicationDate")).thenReturn("2000-12-12");
+        when(mockedRequest.getParameter("price")).thenReturn("10");
+        when(mockedRequest.getParameter("currency")).thenReturn("uan");
+        when(mockedRequest.getParameter("count")).thenReturn("7");
+        when(mockedBookService.findById(1L)).thenReturn(Optional.of(mockedBook));
+        when(mockedBookService.updateBook(any(Book.class))).thenReturn(true);
+
+        String expected = "/user/admin/bookForm.jsp?id="+id+"&successCreation=true";
+        String actual = editBook.execute(mockedRequest);
+
+        assertEquals(expected, actual);
+    }
 }
